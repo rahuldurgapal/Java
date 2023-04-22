320f955c3e60cfe6ba3091eb96fbf639bdd7ee69
rahuldurgapal <rahuldurgapal191@gmail.com>
2023-04-22 20:25:52 +0530

    add

---
+++ Box.java
@@ -0,0 +1,23 @@
+class Box{
+    
+    private int length, breadth, height;
+    public void setDimension(int l, int b, int h)
+    {
+        length=l;
+        breadth=b;
+        height=h;
+    }
+    public void showDimension(){
+        System.out.println("length=" +length);
+        System.out.println("breadth=" +breadth);
+        System.out.println("height=" +height);
+    }
+
+    
+        public static void main(String[] args){
+
+            Box b1=new Box();
+            b1.setDimension(10,12,15);
+            b1.showDimension();
+        }
+}   

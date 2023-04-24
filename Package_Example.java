b78fb935617cadc8a1c06a28459495c68d34e6fd
rahuldurgapal <rahuldurgapal191@gmail.com>
2023-04-24 23:18:52 +0530

    add

---
+++ Package_Example.java
@@ -0,0 +1,10 @@
+//Example of Package in Java
+
+package world;
+public class Package_Example
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World");
+    }
+}
